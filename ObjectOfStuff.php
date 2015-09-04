543bf5a69f2f3687cb7ca502177de354e1267506
Vovaxs <v.sergeyev@hotmail.com>
2015-09-04 22:11:17 +0100

    Tutorial dl9 tebe sone4ko :)

---
+++ ObjectOfStuff.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace NetRacoon;
+/**
+ * Created by PhpStorm.
+ * User: Vovaxs
+ * Date: 04/09/2015
+ * Time: 21:43
+ */
+interface ObjectOfStuff
+{
+
+    /**
+     * @return string
+     */
+    public function getName();
+
+    /**
+     * @return string
+     */
+    public function getId();
+
+}
