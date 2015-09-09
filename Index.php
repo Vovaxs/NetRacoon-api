d20fdb97ab3139faff9236f31743a8b3307c42bd
Vovaxs <v.sergeyev@hotmail.com>
2015-09-09 21:30:45 +0100

    Initial API Structure using Silex: creating app, creating index, using composer, can test by: /demo can test by: /demo/{id}

--- Index.php
+++ Index.php
@@ -1,8 +1,27 @@
 <?php
 /**
  * Created by PhpStorm.
- * Author: Zirka
- * Date: 02.09.2015
- * Time: 22:36
+ * Author: Vovaxs
+ * Date: 09/09/2015
+ * Time: 19:49
  */
-echo"Hello World";
+
+use Silex\Application;
+
+require_once "vendor/autoload.php";
+$app = new \Silex\Application();
+$app['debug'] = true;
+
+$app->get('/', function(){
+    return new Symfony\Component\HttpFoundation\Response("Hello world");
+});
+
+$app->get('/demo', function(){
+    return new Symfony\Component\HttpFoundation\Response("Demo Ta DA!!!!");
+});
+
+$app->get("/demo/{id}", function($id){
+    return "ID - {$id}";
+});
+
+$app->run();
